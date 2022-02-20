9ab360aba02da7c363d3d35415acb5e25db876cf
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2022-02-20 22:18:20 +0800

    Day2_strin_and_strout_java.java

---
+++ Day2_strin_and_strout_java.java.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int s = scan.nextInt();
+        int d = scan.nextInt();
+        
+        System.out.println(a);
+        System.out.println(s);
+        System.out.println(d);
+    }
+}
+		
