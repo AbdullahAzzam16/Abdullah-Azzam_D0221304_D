7e0723ef57f4e68cdc590ca9155c8d203434816f
AbdullahAzzam16 <99625317+AbdullahAzzam16@users.noreply.github.com>
2022-02-24 11:46:51 +0800

    Create Day6_loop_I.java

---
+++ Day6_loop_I.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for(int i=1;i<=10;i++){
+            int result = N * i;
+            System.out.println(N+" x "+i+" = "+result);
+        }  
+          
+        bufferedReader.close();
+    }
+}
